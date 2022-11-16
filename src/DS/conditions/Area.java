20fee8846e9bcfdcc1c1c3942b0897bb440f047c
sbhongade598@gmail.com <sbhongade598@gmail.com>
2022-11-16 13:39:33 +0900

    mster

--- src/DS/conditions/Area.java
+++ src/DS/conditions/Area.java
@@ -4,12 +4,48 @@ import java.util.Scanner;
 
 
 public class Area {
-    public static void main(String[] args) {
 
-        final double PI = Math.PI;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("please input radius");
-        double radius = sc.nextDouble();
-        System.out.println("Area of circel is " + String.format("%.3f",PI * radius * radius));
+    final double PI = Math.PI;
+    double radius;
+    int base;
+    int height;
+    int length;
+    int diagonal_1;
+    int diagonal_2;
+
+
+    public void areaCicle(int radiuss) {
+        this.radius = radiuss;
+        System.out.println("Area of Circle is " + String.format("%.3f", PI * radius * radius));
     }
+    public void areaTriangle(int base,int height){
+        System.out.println("Area of Triangle " + base *height/2);
+    }
+    public void areaEquTriangle(int base){
+        this.base = base;
+        System.out.println("Area of Equilateral Triangle " +  String.format("%.3f", (Math.sqrt(3)*Math.pow(base,2))/4));
+    }
+
+    public void areaRectangle(int length,int breadth){
+        this.length = length;
+        this.height = breadth;
+        System.out.println("Area of Reactangle " + length * height);
+    }
+
+    public void areaParellogram(int base,int height){
+        this.base = base;
+        this.height =height;
+        System.out.println("Area of parellogram " + base *height);
+    }
+
+    public void areaRhombus(int diagonal_1,int diagonal_2){
+        this.diagonal_1 = diagonal_1;
+        this.diagonal_2 = diagonal_2;
+        System.out.println("Area of rhombus " +   (diagonal_1 * diagonal_2)/2);
+    }
+
+
+
+
+
 }
---
+++ src/DS/conditions/Master.java
@@ -0,0 +1,15 @@
+package DS.conditions;
+
+public class Master {
+    public static void main(String[] args) {
+
+        Area obj = new Area();
+
+        obj.areaCicle(3);
+        obj.areaTriangle(4,8);
+        obj.areaEquTriangle(2);
+        obj.areaRectangle(2,6);
+        obj.areaParellogram(5,4);
+        obj.areaRhombus(3,4);
+    }
+}
