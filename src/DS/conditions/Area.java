c62acd780bc387c5caff476372b6dbcc3fea4e84
sbhongade598@gmail.com <sbhongade598@gmail.com>
2022-11-21 10:12:42 +0900

    Arear otimised

--- src/DS/conditions/Area.java
+++ src/DS/conditions/Area.java
@@ -7,41 +7,33 @@ public class Area {
 
     final double PI = Math.PI;
     double radius;
-    int base;
-    int height;
-    int length;
-    int diagonal_1;
-    int diagonal_2;
+    int horizontal;
+    int verticle;
 
 
-    public void areaCicle(int radiuss) {
-        this.radius = radiuss;
+
+    public void areaCicle(double radius) {
+        this.radius = radius;
         System.out.println("Area of Circle is " + String.format("%.3f", PI * radius * radius));
     }
-    public void areaTriangle(int base,int height){
-        System.out.println("Area of Triangle " + base *height/2);
-    }
+
     public void areaEquTriangle(int base){
-        this.base = base;
-        System.out.println("Area of Equilateral Triangle " +  String.format("%.3f", (Math.sqrt(3)*Math.pow(base,2))/4));
+        this.horizontal = base;
+        System.out.println("Area of Equilateral Triangle " +  String.format("%.3f", (Math.sqrt(3)*Math.pow(horizontal,2))/4));
     }
 
-    public void areaRectangle(int length,int breadth){
-        this.length = length;
-        this.height = breadth;
-        System.out.println("Area of Reactangle " + length * height);
-    }
+    public int areabh(int length,int breadth){
+        this.horizontal = length;
+        this.verticle = breadth;
+        int result = horizontal * verticle;
+        return result;
 
-    public void areaParellogram(int base,int height){
-        this.base = base;
-        this.height =height;
-        System.out.println("Area of parellogram " + base *height);
     }
-
-    public void areaRhombus(int diagonal_1,int diagonal_2){
-        this.diagonal_1 = diagonal_1;
-        this.diagonal_2 = diagonal_2;
-        System.out.println("Area of rhombus " +   (diagonal_1 * diagonal_2)/2);
+    public int areaTriRho(int diagonal_1,int diagonal_2){
+        this.horizontal = diagonal_1;
+        this.verticle = diagonal_2;
+        int result = (horizontal * verticle)/2;
+        return result;
     }
 
 
--- src/DS/conditions/Master.java
+++ src/DS/conditions/Master.java
@@ -1,15 +1,71 @@
 package DS.conditions;
 
+import java.util.Scanner;
+
 public class Master {
     public static void main(String[] args) {
 
-        Area obj = new Area();
+        int b;
+        int h;
+        int result;
 
-        obj.areaCicle(3);
-        obj.areaTriangle(4,8);
-        obj.areaEquTriangle(2);
-        obj.areaRectangle(2,6);
-        obj.areaParellogram(5,4);
-        obj.areaRhombus(3,4);
+        Area obj = new Area();
+        System.out.println("select the number for which you want to find area of :-");
+        System.out.println("1.circle");
+        System.out.println("2.Triangle");
+        System.out.println("3.Euilateral Triangle");
+        System.out.println("4.Reactangle");
+        System.out.println("5.Parellelogram");
+        System.out.println("6.Rhombus");
+
+        Scanner sc = new Scanner(System.in);
+        byte selected = sc.nextByte();
+        switch(selected){
+            case 1 -> {
+                System.out.println("please provide radius");
+                double r = sc.nextDouble();
+                obj.areaCicle(r);
+            }
+            case 2 -> {
+                System.out.println("please provide base");
+                b = sc.nextInt();
+                System.out.println("please provide height");
+                h = sc.nextInt();
+                result = obj.areaTriRho(b, h);
+                System.out.println("Area of Triangle " + result);
+            }
+            case 3 -> {
+                System.out.println("please provide base");
+                b = sc.nextInt();
+                obj.areaEquTriangle(b);
+            }
+            case 4 -> {
+                System.out.println("please provide length");
+                b = sc.nextInt();
+                System.out.println("please provide height");
+                h = sc.nextInt();
+                result = obj.areabh(b, h);
+                System.out.println("Area of Reactangle " + result);
+            }
+            case 5 -> {
+                System.out.println("please provide base");
+                b = sc.nextInt();
+                System.out.println("please provide height");
+                h = sc.nextInt();
+                result = obj.areabh(b, h);
+                System.out.println("Area of parellogram " + result);
+            }
+            case 6 -> {
+                System.out.println("please provide diagonal1");
+                b = sc.nextInt();
+                System.out.println("please provide diagonal2");
+                h = sc.nextInt();
+                result = obj.areaTriRho(b, h);
+                System.out.println("Area of rhombus " + result );
+            }
+            default -> System.out.println("please provide valid input");
+        }
+
+
     }
 }
